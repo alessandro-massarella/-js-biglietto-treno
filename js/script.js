56e5fafb37cc0efa7f1b318cb85bde0dc2a576ea
alessandro <alessandro.massarella@gmail.com>
2020-10-20 16:36:35 +0200

    1.0

--- js/script.js
+++ js/script.js
@@ -4,4 +4,8 @@ var priceInit = (distance * 0,21);
 
 if (age < 19) {
   document.getElementById ('minor').innerHTML = ('Hai meno di 18 anni, il prezzo, comprensivo di sconto del 20%, è di Euro: ' + (priceInit * 0.8));
+} else if (age < 65) {
+  document.getElementById ('major').innerHTML = ('Il prezzo è di Euro: ' + priceInit);
+} else if (age > 64) {
+  document.getElementById ('over').innerHTML = ('Hai più di 65 anni, il prezzo, comprensivo di sconto del 40%, è di Euro: ' + (priceInit * 0.6));
 }
