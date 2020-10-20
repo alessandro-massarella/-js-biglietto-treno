7be5d83a6b1d3a61b4c386fb069a569859f6a071
alessandro <alessandro.massarella@gmail.com>
2020-10-20 17:15:49 +0200

    Fix vari

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var distance = prompt ('Quanti km devi percorrere? ');
 var age = prompt ('Quanti anni hai? ');
-var priceInit = (distance * 0,21);
+var priceInit = (distance * 0,21).toFixed(2);
+var minorDiscount = (priceInit - (priceInit * 0.2)).toFixed(2);
+var overDiscount = (priceInit - (priceInit * 0.4)).toFixed(2);
 
 if (age < 19) {
-  document.getElementById ('minor').innerHTML = ('Hai meno di 18 anni, il prezzo, comprensivo di sconto del 20%, è di Euro: ' + (priceInit * 0.8));
+  document.getElementById ('minor').innerHTML = ('Hai meno di 18 anni, il prezzo, comprensivo di sconto del 20%, è di Euro: ' + minorDiscount);
 } else if (age < 65) {
   document.getElementById ('major').innerHTML = ('Il prezzo è di Euro: ' + priceInit);
 } else if (age > 64) {
-  document.getElementById ('over').innerHTML = ('Hai più di 65 anni, il prezzo, comprensivo di sconto del 40%, è di Euro: ' + (priceInit * 0.6));
+  document.getElementById ('over').innerHTML = ('Hai più di 65 anni, il prezzo, comprensivo di sconto del 40%, è di Euro: ' + overDiscount);
 }
